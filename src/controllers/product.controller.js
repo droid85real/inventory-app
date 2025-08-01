08786c147318db6d3e781f28418a7c4a68777607
tanishk kushwaha <eragonx8555@gmail.com>
2025-08-01 12:43:44 +0530

    Validating form data, server side and html form and also repopulate on error

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,13 +14,42 @@ export default class ProductController {
 
   //this function, so user can get form
   getAddForm(req, res) {
-    return res.render("new-product");
+    return res.render("new-product", { errorMessage: null, formData: {} }); //rendering new-product.ejs page
   }
 
   addNewForm(req, res) {
     // let products = ProductModel.get();
     // console.log(req.body);
-    ProductModel.add(req.body);
+
+    //validate data
+    const { name, description, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() == "") {
+      errors.push("Name is required");
+    }
+
+    if (!description || description.trim() == "") {
+      errors.push("Description is reqrired");
+    }
+
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price must be positive");
+    }
+
+    try {
+      new URL(imageUrl);
+    } catch (err) {
+      errors.push("Image url is invalid");
+    }
+
+    if (errors.length > 0) {
+      return res.render("new-product", {
+        errorMessage: errors, //passing error msg to new-product.ejs
+        formData: req.body, //to repopulate with old data when error occur
+      });
+    }
+
+    ProductModel.add(req.body); //passing data we received
 
     // res.render("products", { products: products });
     res.redirect("/"); //redirect after post to prevent resubmission on refresh
